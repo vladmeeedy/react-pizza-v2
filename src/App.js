f695d3f602dc6a8662bc02ed48eec1876e71f746
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-01-20 21:36:39 +0200

    аdaptive layoute

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import './scss/app.scss';
-import Header from './components/Header.jsx';
-import React from 'react';
-import pizzas from './assets/pizzas.json';
-import Home from './pages/Home.jsx';
-import Cart from './pages/Cart.jsx';
-import NotFound from './pages/NotFound.jsx';
-import { Route, Routes } from 'react-router-dom';
+import './scss/app.scss'
+import Header from './components/Header.jsx'
+import React from 'react'
+import pizzas from './assets/pizzas.json'
+import Home from './pages/Home.jsx'
+import Cart from './pages/Cart.jsx'
+import NotFound from './pages/NotFound.jsx'
+import { Route, Routes } from 'react-router-dom'
 
-console.log(pizzas);
+console.log(pizzas)
 
 function App() {
-
-
   return (
     <div className="wrapper">
-      <Header/>
+      <Header />
       <div className="content">
         <div className="container">
           <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/cart' element={<Cart/>}/>
-            <Route path='*' element={<NotFound/>}/>
-          </Routes>         
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
