80246ca1d539e2d0b1dfd57cdb6b2a9717161ccf
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2024-01-21 19:46:07 +0200

    add cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,37 @@
-import React from 'react';
-import Categories from '../components/Categories';
-import Sort from '../components/Sort';
-import PizzaBlock from '../components/PizzaBlock';
-import Skeleton from '../components/PizzaBlock/Skeleton.jsx';
+import React from 'react'
+import Categories from '../components/Categories'
+import Sort from '../components/Sort'
+import PizzaBlock from '../components/PizzaBlock'
+import Skeleton from '../components/PizzaBlock/Skeleton.jsx'
 
 const Home = () => {
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
-  
-    React.useEffect(()=>{
-      fetch('https://65a92c59219bfa371868aad2.mockapi.io/items')
-    .then((res)=> res.json())
-    .then((arr)=>{
-      setItems(arr);
-      setIsLoading(false)
-    })
-    }, [])
-    
-    return (
-        <>
-         <div className="content__top">
-          <Categories/>
-          <Sort/>
-          </div>
-          <h2 className="content__title">Всі піци</h2>
-          <div className="content__items">
-            {isLoading 
-            ? [...new Array(6)].map((_, index)=> <Skeleton key={index}/>) 
-            : items.map(obj =><PizzaBlock key={obj.id} {...obj}/>)
-            }
-          
-          </div>
-            
-        </>
-    );
-};
+  const [items, setItems] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
 
-export default Home;
+  React.useEffect(() => {
+    fetch('https://65a92c59219bfa371868aad2.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr)
+        setIsLoading(false)
+      })
+      window.scrollTo(0, 0)
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Всі піци</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  )
+}
+
+export default Home
